48ee3d2342b5c0c1ed7d8dc44efd40c58d7c5f1b
kishor <kishorrathva8298@gmail.com>
2019-01-30 23:01:55 +0530

    js

--- test.js
+++ test.js
@@ -1,15 +1,42 @@
-"use strict"
-let arr = [
-			1,
-			2,
-			kishor,
-			test,
-			a :
-				function(a,b){
-					return (a+b);
+let data = {}
+		let count = 1;
+		let key = null;
+		let arr = [
+		1,
+		2,
+		10,
+		"kishor",
+		"test",
+		{
+			a:{
+				b:function(){},
+				c:"Hello",
+				d:10
 			}
-			];
+		}
+		];
+		let res=[];
+		console.log(typeof(arr[5]));
+		for(a in arr){
 
-for a in arr{
-	alert (a);
-}
+			if(typeof(arr[a]) == 'number' ){
+				data["key"] = arr[a] ;
+				data["count"] =  arr[a] + count;
+				count ++ ;
+				res.push(data);
+				data = {};
+			}else if(typeof(arr[a]) == 'string' ){
+				data["key"] = arr[a] ;
+				data["NAME"] =  arr[a] 
+				res.push(data);
+				data = {};
+			}else if(typeof(arr[a] ) == 'object' ){
+				key =arr[a] ;
+				data[key] = Object.keys(arr[a]);
+			}else{
+				alert("sorry");
+			}
+			// alert(arr[a]);
+		}
+
+		console.log(res);
