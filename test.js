c9903f98e634ab2ce106d63744683e7c05a21e36
kishor <kishorrathva8298@gmail.com>
2019-01-31 00:15:15 +0530

    Added Parser for objects

--- test.js
+++ test.js
@@ -1,42 +1,40 @@
 let data = {}
-		let count = 1;
-		let key = null;
-		let arr = [
-		1,
-		2,
-		10,
-		"kishor",
-		"test",
-		{
-			a:{
-				b:function(){},
-				c:"Hello",
-				d:10
+let count = 1;
+let key = null;
+let arr = [1,2,10,"kishor", "test",{a:{ b:function(){}, c:"Hello",d:10 } } ];
+let res=[];
+
+for(a in arr){
+
+	if(typeof(arr[a]) == 'number' ){
+		data["key"] = arr[a] ;
+		data["count"] =  arr[a] + count;
+		count ++ ;
+		res.push(data);
+		data = {};
+	}else if(typeof(arr[a]) == 'string' ){
+		data["key"] = arr[a] ;
+		data["name"] =  arr[a] 
+		res.push(data);
+		data = {};
+	}else if(typeof(arr[a] ) == 'object' ){
+		let obj = arr[a];
+		for(let i in obj){
+			let  datacopy = obj[i];
+			data["key"] = Object.keys(obj);
+			for ( let k in datacopy){
+				data["subkey"] = k ;
+				data["subValue"] = (obj[i][k]) ;
+				res.push(data);
+				data = {};
 			}
 		}
-		];
-		let res=[];
-		console.log(typeof(arr[5]));
-		for(a in arr){
-
-			if(typeof(arr[a]) == 'number' ){
-				data["key"] = arr[a] ;
-				data["count"] =  arr[a] + count;
-				count ++ ;
-				res.push(data);
-				data = {};
-			}else if(typeof(arr[a]) == 'string' ){
-				data["key"] = arr[a] ;
-				data["NAME"] =  arr[a] 
-				res.push(data);
-				data = {};
-			}else if(typeof(arr[a] ) == 'object' ){
-				key =arr[a] ;
-				data[key] = Object.keys(arr[a]);
-			}else{
-				alert("sorry");
-			}
+	}else{
+			return console.log("What Happened ??!");
+	}
 			// alert(arr[a]);
 		}
 
 		console.log(res);
+		console.log(arr[5]);
+		
