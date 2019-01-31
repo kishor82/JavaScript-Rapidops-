b2b77b819c624bdb9496d6d59ef0f6cb4ea37ab1
kishor <kishorrathva8298@gmail.com>
2019-01-31 20:29:18 +0530

    Modified test.js

--- test.js
+++ test.js
@@ -1,40 +1,66 @@
 let data = {}
-let count = 1;
-let key = null;
-let arr = [1,2,10,"kishor", "test",{a:{ b:function(){}, c:"Hello",d:10 } } ];
-let res=[];
+		let count = 1;
+		let key = null;
+		let arr = [1,
+		2,
+		10,
+		"kishor",
+		"test",
+		{
+			a:
+			{ b:function(){}
+			, c:"Hello",d:{name:"kishor"} 
+		},
+		f:
+		{name:"kishor"}
+		}];
+		let res=[];
 
-for(a in arr){
+		for(a in arr){
 
-	if(typeof(arr[a]) == 'number' ){
-		data["key"] = arr[a] ;
-		data["count"] =  arr[a] + count;
-		count ++ ;
-		res.push(data);
-		data = {};
-	}else if(typeof(arr[a]) == 'string' ){
-		data["key"] = arr[a] ;
-		data["name"] =  arr[a] 
-		res.push(data);
-		data = {};
-	}else if(typeof(arr[a] ) == 'object' ){
-		let obj = arr[a];
-		for(let i in obj){
-			let  datacopy = obj[i];
-			data["key"] = Object.keys(obj);
-			for ( let k in datacopy){
-				data["subkey"] = k ;
-				data["subValue"] = (obj[i][k]) ;
+			if(typeof(arr[a]) == 'number' ){
+				data["key"] = arr[a] ;
+				data["count"] =  arr[a] + count;
+				count ++ ;
 				res.push(data);
 				data = {};
+			}else if(typeof(arr[a]) == 'string' ){
+				data["key"] = arr[a] ;
+				data["name"] =  arr[a] 
+				res.push(data);
+				data = {};
+			}else if(typeof(arr[a] ) == 'object' ){
+				let obj = arr[a];
+				let inner = {} ;
+				// let c = 1 ;
+				// let In = 1 ;
+				for(let i in obj){
+					// this is to see how loop iterate 
+					// console.log(c+" - iteration");
+					// console.log(i);
+					// console.log(typeof(obj[i]));
+					let  datacopy = obj[i];
+
+					data[`key-${i}`] = i;
+					for ( let k in datacopy){
+						// console.log("["+c+"]"+"["+In+"] -  iteration ");
+						// console.log (k)
+						inner[`key-${k}`] = k ;
+						inner[`value-${k}`] = (obj[i][k]) ;
+						data[`Value-${k}`] = inner ;
+						// console.log(data);
+						inner = {};
+						// In  ++;
+					}
+					res.push(data);
+					data = {};
+					// c ++;
+					// In = 1 ;
+				}
+				
+			}else{
+
 			}
-		}
-	}else{
-			return console.log("What Happened ??!");
-	}
 			// alert(arr[a]);
 		}
-
 		console.log(res);
-		console.log(arr[5]);
-		
